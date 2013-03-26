25d61bbc9badb27ca25f285f3e39e74b099a7b74
xiaoxiao-geng <GengXiaoXiao@3gwin.cn>
2013-03-26 17:56:20 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,8 @@ local text = load("text.txt")
 
 local first = text[1]
 for j = 1 , 100 do
-for i = 1, 4 do
-	print ("i", i)
-end
+	for i = 1, 4 do
+		print ( "i", i )
+		print ( "i2", i * 2 )
+	end
 end
